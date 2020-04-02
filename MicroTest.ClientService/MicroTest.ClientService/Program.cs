ff2a7e793c353469c1b442ef6dffd07c90898549
Adrian smitto <adriansmitto@yahoo.com.ar>
2020-04-01 23:56:48 -0500

    First run

---
+++ MicroTest.ClientService/MicroTest.ClientService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MicroTest.ClientService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
